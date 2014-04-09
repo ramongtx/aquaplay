e559d14332acf280746bb2d31c182a1450290c50
ramongtx <ramongtx@gmail.com>
2014-04-09 15:23:54 -0300

    Progress on rings

---
+++ AquaPlay/RingView.h
@@ -0,0 +1,13 @@
+//
+//  RingView.h
+//  AquaPlay
+//
+//  Created by Ramon Carvalho Maciel on 4/9/14.
+//  Copyright (c) 2014 Rock Bottom. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RingView : UIView
+
+@end
