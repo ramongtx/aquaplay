501c5774856d36aa24629500133c6a7fc51cc3c1
ramongtx <ramongtx@gmail.com>
2014-04-09 15:24:16 -0300

    Progress on rings...

--- AquaPlay/RingView.m
+++ AquaPlay/RingView.m
@@ -15,17 +15,81 @@
     self = [super initWithFrame:frame];
     if (self) {
         // Initialization code
+        [self initiateRing];
     }
     return self;
 }
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect
+#pragma mark - View Generator
+
+UIColor* color;
+
+- (void) initiateRing
 {
-    // Drawing code
+    // drop shadow
+    [self.layer setShadowColor:[UIColor blackColor].CGColor];
+    [self.layer setShadowOpacity:0.1];
+    [self.layer setShadowRadius:3.0];
+    [self.layer setShadowOffset:CGSizeMake(2.0, 2.0)];
+    
+    if (!self.left) {
+        CGRect rect = CGRectApplyAffineTransform(self.frame, CGAffineTransformMakeScale(0.25, 1));
+        rect.origin.x = 0;
+        rect.origin.y = 0;
+        self.left = [[UIView alloc] initWithFrame:rect];
+        [self addSubview:self.left];
+    }
+    
+    if (!self.right) {
+        CGRect rect = CGRectApplyAffineTransform(self.frame, CGAffineTransformMakeScale(0.25, 1));
+        rect.origin.x = self.frame.size.width - rect.size.width;
+        rect.origin.y = 0;
+        self.right = [[UIView alloc] initWithFrame:rect];
+        [self addSubview:self.right];
+    }
+    
+    [self setColor:[self randomColor]];
 }
-*/
+
+#pragma mark - Colors
+
+- (UIColor*) randomColor
+{
+    CGFloat hue = ( arc4random() % 256 / 256.0 ); // 0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5; // 0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5; // 0.5 to 1.0, away from black
+    return [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+}
+
+- (UIColor *)lighterColor:(UIColor*) c
+{
+    CGFloat h, s, b, a;
+    if ([c getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:MIN(b * 1.3, 1.0)
+                               alpha:a];
+    return nil;
+}
+
+- (UIColor *)darkerColor:(UIColor*) c
+{
+    CGFloat h, s, b, a;
+    if ([c getHue:&h saturation:&s brightness:&b alpha:&a])
+        return [UIColor colorWithHue:h
+                          saturation:s
+                          brightness:b * 0.75
+                               alpha:a];
+    return nil;
+}
+
+- (void) setColor:(UIColor *)newColor
+{
+    color = newColor;
+    self.backgroundColor = color;
+    self.left.backgroundColor = [self darkerColor:color];
+    self.right.backgroundColor = [self darkerColor:color];
+
+}
 
 @end
--- AquaPlay/ViewController.m
+++ AquaPlay/ViewController.m
@@ -7,21 +7,23 @@
 //
 
 #import "ViewController.h"
+#import "RingView.h"
+
+#define RING_WIDTH 60
+#define RING_HEIGHT 20
 
 @interface ViewController ()
-@property (weak, nonatomic) IBOutlet UIView *objView1;
-@property (weak, nonatomic) IBOutlet UIView *objView2;
-@property (weak, nonatomic) IBOutlet UIView *objView3;
-@property (weak, nonatomic) IBOutlet UIView *objView4;
-@property (weak, nonatomic) IBOutlet UIView *objView5;
 @property (strong) UIDynamicAnimator* animator;
 @property (strong) UIGravityBehavior* gravity;
-@property (strong) UICollisionBehavior* collision;
+@property (strong) UICollisionBehavior* collisionForeground;
+@property (strong) UICollisionBehavior* collisionBackground;
+
+@property (weak, nonatomic) IBOutlet UIView *objView1;
 @property (weak, nonatomic) IBOutlet UIView *obstacle1;
 @property (weak, nonatomic) IBOutlet UIView *obstacle2;
 @property (weak, nonatomic) IBOutlet UIView *obstacle3;
 @property (strong) NSMutableArray* obstacles;
-@property (strong) NSMutableArray* foregroundObjects;
+@property (strong) NSMutableArray* collisionGroup;
 @property (strong) NSMutableArray* etherealObjects;
 @property (strong) NSMutableArray* foregroundCollisionArray;
 @property (strong) NSMutableArray* allObjects;
@@ -31,6 +33,9 @@
 @property (weak, nonatomic) IBOutlet UIView *ringRight;
 @property (weak, nonatomic) IBOutlet UIView *ringCenter;
 
+@property (strong) NSMutableArray* ringArray;
+@property (strong) NSMutableArray* leftArray;
+@property (strong) NSMutableArray* rightArray;
 
 @end
 
@@ -40,62 +45,68 @@
 {
     [super viewDidLoad];
     
-    self.obstacles = [[NSMutableArray alloc] initWithArray:@[self.obstacle1,self.obstacle2,self.obstacle3]];
-    self.foregroundObjects = [[NSMutableArray alloc] initWithArray:@[self.objView1,self.objView2,self.objView3,self.objView4,self.objView5,self.ringLeft,self.ringRight]];
-    self.etherealObjects = [[NSMutableArray alloc] initWithArray:@[self.ringCenter]];
-    
-    self.foregroundCollisionArray = [[NSMutableArray alloc] initWithArray:self.obstacles];
-    [self.foregroundCollisionArray addObjectsFromArray:self.foregroundObjects];
-    
-    self.allObjects = [[NSMutableArray alloc] initWithArray:self.foregroundCollisionArray];
-    [self.allObjects addObjectsFromArray:self.etherealObjects];
-    
-	// Do any additional setup after loading the view, typically from a nib.
     self.animator = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
-    self.gravity = [[UIGravityBehavior alloc] initWithItems:self.foregroundObjects];
-    self.collision = [[UICollisionBehavior alloc] initWithItems:self.foregroundCollisionArray];
-    self.dynamicBehavior = [[UIDynamicItemBehavior alloc] initWithItems:self.allObjects];
     
+    self.ringArray = [[NSMutableArray alloc] init];
+    self.leftArray = [[NSMutableArray alloc] init];
+    self.rightArray = [[NSMutableArray alloc] init];
+    [self createRings:3];
+ 
+    self.gravity = [[UIGravityBehavior alloc] initWithItems:self.ringArray];
+    self.gravity.magnitude = 0.5;
+    [self.animator addBehavior:self.gravity];
+
+    self.collisionBackground = [[UICollisionBehavior alloc] initWithItems:self.ringArray];
+    self.collisionBackground.translatesReferenceBoundsIntoBoundary = YES;
+    [self.animator addBehavior:self.collisionBackground];
+
+    self.dynamicBehavior = [[UIDynamicItemBehavior alloc] initWithItems:self.ringArray];
+    self.dynamicBehavior.density = 0.3;
+    self.dynamicBehavior.elasticity = 0.5;
+    [self.animator addBehavior:self.dynamicBehavior];
+
+
     for (UIView *obstacle in self.obstacles) {
         UIAttachmentBehavior *attachment = [[UIAttachmentBehavior alloc] initWithItem:obstacle attachedToAnchor:obstacle.center];
         [self.animator addBehavior:attachment];
     }
+//
+//    for (RingView* ring in self.ringArray) {
+//        UIAttachmentBehavior *leftAttachment = [[UIAttachmentBehavior alloc] initWithItem:ring.left attachedToItem:ring];
+//        [self.animator addBehavior:leftAttachment];
+//        UIAttachmentBehavior *rightAttachment = [[UIAttachmentBehavior alloc] initWithItem:ring.right attachedToItem:ring];
+//        [self.animator addBehavior:rightAttachment];
+//    }
+//
     
+    
+    
+    self.obstacles = [[NSMutableArray alloc] initWithArray:@[self.obstacle1,self.obstacle2,self.obstacle3]];
+    
+    self.collisionGroup = [[NSMutableArray alloc] initWithArray:self.obstacles];
+//    [self.collisionGroup addObjectsFromArray:self.leftArray];
+//    [self.collisionGroup addObjectsFromArray:self.rightArray];
+    
+    self.collisionForeground = [[UICollisionBehavior alloc] initWithItems:self.collisionGroup];
+    [self.animator addBehavior:self.collisionForeground];
+
     UIDynamicItemBehavior *itemBehavior = [[UIDynamicItemBehavior alloc] initWithItems:self.obstacles];
     itemBehavior.allowsRotation = NO;
     [self.animator addBehavior:itemBehavior];
     
-    self.gravity.magnitude = 0.5;
-    self.collision.translatesReferenceBoundsIntoBoundary = YES;
-    self.dynamicBehavior.elasticity = 0.5;
-    
-    [self.animator addBehavior:self.gravity];
-    [self.animator addBehavior:self.collision];
-    [self.animator addBehavior:self.dynamicBehavior];
     
     
-    UIAttachmentBehavior* attach = [[UIAttachmentBehavior alloc] initWithItem:self.ringLeft attachedToItem:self.ringRight];
-    attach.damping = 0;
-    [self.animator addBehavior:attach];
     
-    attach = [[UIAttachmentBehavior alloc] initWithItem:self.ringCenter attachedToItem:self.ringRight];
-    attach.damping = 0;
-    [self.animator addBehavior:attach];
-    
-    attach = [[UIAttachmentBehavior alloc] initWithItem:self.ringCenter attachedToItem:self.ringLeft];
-    attach.damping = 0;
-    [self.animator addBehavior:attach];
-    
+    // Tap Gesture Recognizer
     UITapGestureRecognizer* singleTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleSingleTap:)];
     
     singleTap.numberOfTapsRequired = 1;
     singleTap.numberOfTouchesRequired = 1;
     [self.view addGestureRecognizer: singleTap];
-    
 }
 
 - (IBAction)bottomLeftButton:(id)sender {
-    for (UIView *obj in self.foregroundObjects) {
+    for (UIView *obj in self.ringArray) {
         UIPushBehavior* pushBehavior;
         pushBehavior = [[UIPushBehavior alloc] initWithItems:@[obj] mode:UIPushBehaviorModeInstantaneous];
         pushBehavior.magnitude = 0.0f;
@@ -112,12 +123,12 @@
         
         pushBehavior.pushDirection = CGVectorMake(originForce*distanceVector.y,2*originForce*distanceVector.x);
         pushBehavior.active = YES;
-
+        
     }
 }
 
 - (IBAction)bottomRightButton:(id)sender {
-    for (UIView *obj in self.foregroundObjects) {
+    for (UIView *obj in self.ringArray) {
         UIPushBehavior* pushBehavior;
         pushBehavior = [[UIPushBehavior alloc] initWithItems:@[obj] mode:UIPushBehaviorModeInstantaneous];
         pushBehavior.magnitude = 0.0f;
@@ -191,5 +202,39 @@
     NSLog(@"handleSingleTap");
 }
 
+- (void) createRingAtPosition:(CGPoint)point
+{
+    RingView* ring = [[RingView alloc] initWithFrame:CGRectMake(point.x, point.y, RING_WIDTH, RING_HEIGHT)];
+    [self.view addSubview:ring];
+    [self.ringArray addObject:ring];
+    [self.leftArray addObject:ring.left];
+    [self.rightArray addObject:ring.right];
+}
+
+-(void) createRings:(int)number
+{
+    int numViews = 0;
+    while (numViews < number) {
+        BOOL goodView = YES;
+        RingView *ring = [[RingView alloc] initWithFrame:CGRectMake(arc4random() % (int)self.view.frame.size.width, arc4random() % (int)self.view.frame.size.height, RING_WIDTH, RING_HEIGHT)];
+        if (!CGRectContainsRect(self.view.frame, ring.frame)) {
+            goodView = NO;
+        } else {
+            for (UIView *placedView in self.view.subviews) {
+                if (CGRectIntersectsRect(CGRectInset(ring.frame, -10, -10), placedView.frame)) {
+                    goodView = NO;
+                    break;
+                }
+            }
+        }
+        if (goodView) {
+            [self.view addSubview:ring];
+            [self.ringArray addObject:ring];
+            [self.leftArray addObject:ring.left];
+            [self.rightArray addObject:ring.right];
+            numViews += 1;
+        }
+    }
+}
 
 @end
